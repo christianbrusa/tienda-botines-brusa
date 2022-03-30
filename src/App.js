8a507941ffb8384f38e0224734c36a197288aa27
christianbrusa <christianbrusa@hotmail.com>
2022-03-30 00:23:17 -0300

    Importo componente ItemListContainer en App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
+    <>
     <NavBar></NavBar>
+    <ItemListContainer></ItemListContainer>
+    </>
   );
 }
 
