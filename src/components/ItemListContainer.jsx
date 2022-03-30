003ca4263d4eab1543bdaf1002354d40751c7254
christianbrusa <christianbrusa@hotmail.com>
2022-03-30 00:22:06 -0300

    Agrego componente ItemListContainer

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function ItemListContainer() {
+    return (
+        //Fragments
+        <>
+            <div>
+                <img
+                    src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648259988/obrablue_2_gkgt6n.jpg"
+                    width="100%"
+                    height="400"
+                    className="image1"
+                    alt=""
+                />
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
+        </>
+    )
+} 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,33 +41,6 @@ export default class NavBar extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-                <div>
-                    <img
-                        src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648259988/obrablue_2_gkgt6n.jpg"
-                        width="100%"
-                        height="400"
-                        className="image1"
-                        alt=""
-                    />
-                </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laborum mollitia voluptas explicabo sit vero vel accusamus ipsa, tempore suscipit ad minima sapiente repellendus, quasi ratione necessitatibus aliquam dicta! Iste.</p>
             </div>
         );
     }
