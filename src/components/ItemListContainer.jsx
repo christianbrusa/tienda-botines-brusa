9e96e01cbe751e11e8f93aa13d6ca83378d87767
christianbrusa <christianbrusa@hotmail.com>
2022-04-05 19:18:34 -0300

    Uso componente ItemCount en ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Button, Card } from 'react-bootstrap';
+import ItemCount from "./ItemCount";
 import "../css/ItemListContainer.css";
 
 export default function ItemListContainer({ greeting }) {
+
+    const [stockInicial,setStockInicial] = useState(1);
+    const [stock,setStock] = useState(5);
+
+    function onAdd(numero){
+        setStockInicial(numero);
+    }
+
     return (
         //Fragments
         <>
@@ -25,110 +34,15 @@ export default function ItemListContainer({ greeting }) {
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                         </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto2</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto3</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto4</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto5</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
+                        <div className="count">
+                        <ItemCount stockInicial= {stockInicial} stock= {stock} onAdd = {onAdd}/>
+                        </div>
+                        <Button id="addToCart" variant="outline-primary">Agregar al carrito</Button>
                     </Card.Body>
                 </Card>
             </div>
             <div className="products">
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto6</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto7</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto8</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto9</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto10</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
+                
             </div>
         </>
     )
