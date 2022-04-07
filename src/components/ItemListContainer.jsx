34fe002436177a79bdc8b9aa6f4fa369f33accfd
christianbrusa <christianbrusa@hotmail.com>
2022-04-07 18:47:36 -0300

    Remodelo componente ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,19 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Button, Card } from 'react-bootstrap';
-import ItemCount from "./ItemCount";
+import Promise from '../utils/Promesa';
+import Products from './Products';
+import ItemList from "./ItemList";
 import "../css/ItemListContainer.css";
 
-export default function ItemListContainer({ greeting }) {
+export default function ItemListContainer() {
 
-    const [stockInicial,setStockInicial] = useState(1);
-    const [stock,setStock] = useState(5);
+    const [Items, setItems] = useState([]);
 
-    function onAdd(numero){
-        setStockInicial(numero);
-    }
+    useEffect(() => {
+        Promise(700,Products)
+        .then(result => setItems(result))
+        .catch(error => console.log("error"));
+    }, [Items])
 
     return (
         //Fragments
@@ -19,30 +22,13 @@ export default function ItemListContainer({ greeting }) {
                 <img
                     src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648259988/obrablue_2_gkgt6n.jpg"
                     width="100%"
-                    height="250"
+                    height="190"
                     className="image1"
                     alt=""
                 />
             </div>
-            <p>{greeting}</p>
-            <div className="products">
-                <Card style={{ width: '14rem' }}>
-                    <Card.Img variant="top" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png" style={{ width: 170, height: 170 }} />
-                    <Card.Body>
-                        <Card.Title>Producto1</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <div className="count">
-                        <ItemCount stockInicial= {stockInicial} stock= {stock} onAdd = {onAdd}/>
-                        </div>
-                        <Button id="addToCart" variant="outline-primary">Agregar al carrito</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-            <div className="products">
-                
+            <div className="ResultProducts">
+                <ItemList Products={Items}/>
             </div>
         </>
     )
