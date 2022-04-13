76ac9a386f2c9be3df403beb2dced093cd441a86
christianbrusa <christianbrusa@hotmail.com>
2022-04-12 23:38:49 -0300

    Agrego componente ItemDetailContainer en App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount';
 import Item from './components/Item';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+      
       <ItemListContainer />
+      <NavBar />
+      <ItemDetailContainer/>
+
+      <Routes>
+      </Routes>
+      
+      </BrowserRouter>
     </>
   );
 }
