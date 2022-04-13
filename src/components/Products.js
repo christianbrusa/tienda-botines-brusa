4752c85691e5612e30a462e2793aa56e4f66089d
christianbrusa <christianbrusa@hotmail.com>
2022-04-12 23:37:18 -0300

    Cambio css

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,35 +3,35 @@ const Products = [
         id: 1111,
         title: "Producto 1",
         description: "",
-        price: '$'+20000,
+        price: '$' + 20000,
         pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png"
     },
     {
         id: 1112,
         title: "Producto 2",
         description: "",
-        price: '$'+20000,
+        price: '$' + 20000,
         pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png"
     },
     {
         id: 1113,
         title: "Producto 3",
         description: "",
-        price: '$'+20000,
+        price: '$' + 20000,
         pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png"
     },
     {
         id: 1111,
         title: "Producto 1",
         description: "",
-        price: '$'+20000,
+        price: '$' + 20000,
         pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png"
     },
     {
         id: 1112,
         title: "Producto 2",
         description: "",
-        price: '$'+20000,
+        price: '$' + 20000,
         pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png"
     }
 ]
