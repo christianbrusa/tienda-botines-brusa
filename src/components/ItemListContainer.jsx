c702243f8856267fbd75d199f457af24ae90086b
christianbrusa <christianbrusa@hotmail.com>
2022-04-16 02:12:17 -0300

    Importo y uso useParams y armo logica para filtrar por id

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,31 +4,39 @@ import Promise from '../utils/Promesa';
 import Products from './Products';
 import ItemList from "./ItemList";
 import "../css/ItemListContainer.css";
+import { useParams } from "react-router-dom";
 
 export default function ItemListContainer() {
 
     const [Items, setItems] = useState([]);
 
+    const { id } = useParams();
+
+    let filterProducts = [];
+
     useEffect(() => {
-        Promise(700,Products)
-        .then(result => setItems(result))
-        .catch(error => console.log("error"));
-    }, [Items])
+        //Promise(700,Products)
+
+        if (id) {
+            filterProducts = Products.filter(item => item.category == id)
+            console.log(filterProducts)
+            Promise(700, filterProducts)
+                .then(result => setItems(result))
+                .catch(error => console.log("error"));
+        }
+        else {
+            Promise(700, Products)
+                .then(result => setItems(result))
+                .catch(error => console.log("error"));
+        }
+
+    }, [id])
 
     return (
         //Fragments
         <>
-            <div>
-                <img
-                    src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648259988/obrablue_2_gkgt6n.jpg"
-                    width="100%"
-                    height="190"
-                    className="image1"
-                    alt=""
-                />
-            </div>
             <div className="ResultProducts">
-                <ItemList Products={Items}/>
+                <ItemList Products={Items} />
             </div>
         </>
     )
