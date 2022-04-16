d18c96781631d08a14a8816c292d49c6adc89b47
christianbrusa <christianbrusa@hotmail.com>
2022-04-16 02:11:05 -0300

    Importo y uso react-router-dom

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,9 @@ import CartWidget from "./CartWidget";
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown, Container, Button, Form, FormControl } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 <link href='https://fonts.googleapis.com/css?family=Roboto Condensed' rel='stylesheet'></link>
+
 
 
 export default class NavBar extends Component {
@@ -18,29 +20,37 @@ export default class NavBar extends Component {
                                 width="70"
                                 height="50"
                                 className="d-inline-block"
-                                alt=""
+                                alt="/"
                             />
                         </nav>
-                        <Navbar.Brand href="#" className="brand" id="title">Botines AG</Navbar.Brand>
+                        <Navbar.Brand href="/" className="brand" id="title">Botines AG</Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll" className="d-flex2">
                             <Nav
                                 className="me-auto my-2 my-lg-0"
                                 style={{ maxHeight: '300px' }} //Responsive
                             >
-                                <Nav.Link href="#action1" className="links">Inicio</Nav.Link>
-                                <NavDropdown title="Productos" className="links" id="navbarScrollingDropdown">
-                                    <NavDropdown.Item href="#action3">Nike</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">Adidas</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">Puma</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href="#action2" className="links">FAQ</Nav.Link>
+                                <div className="routes">
+                                <Link to={`/`}>Inicio</Link>
+                                <Link to={`/categoria/nike`}>Nike</Link>
+                                <Link to={`/categoria/adidas`}>Adidas</Link>
+                                <Link to={`/categoria/puma`}>Puma</Link>
+                                </div>
                             </Nav>
                             <CartWidget />
                             <Button variant="light" style={{ marginLeft: 12.5, fontFamily: "Bahnschrift" }}>Login</Button>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+                <div>
+                    <img
+                        src="https://res.cloudinary.com/dycoseuyv/image/upload/v1648259988/obrablue_2_gkgt6n.jpg"
+                        width="100%"
+                        height="190"
+                        className="banner"
+                        alt=""
+                    />
+                </div>
             </div>
         );
     }
