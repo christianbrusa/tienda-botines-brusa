8cf24d39c41df3e262d5b8f97fb97e4c0595244d
christianbrusa <christianbrusa@hotmail.com>
2022-04-16 02:01:09 -0300

    Agrego mas productos al listado

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,36 +4,65 @@ const Products = [
         title: "Producto 1",
         description: "",
         price: '$' + 20000,
-        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png"
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png",
+        category: "nike"
     },
     {
         id: 1112,
         title: "Producto 2",
         description: "",
         price: '$' + 20000,
-        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png"
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png",
+        category: "nike"
     },
     {
         id: 1113,
         title: "Producto 3",
         description: "",
         price: '$' + 20000,
-        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png"
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705321/lightobra-removebg-preview_gxszcs.png",
+        category: "nike"
     },
     {
-        id: 1111,
-        title: "Producto 1",
+        id: 1114,
+        title: "Producto 4",
         description: "",
         price: '$' + 20000,
-        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png"
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1650084711/adidas-copa-19.1_black-removebg-preview_hyzltp.png",
+        category: "adidas"
     },
     {
-        id: 1112,
-        title: "Producto 2",
+        id: 1115,
+        title: "Producto 5",
         description: "",
         price: '$' + 20000,
-        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/orangeobra-removebg-preview_xxwpef.png"
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1650084702/adidas-copa-19.1-removebg-preview_albyjj.png",
+        category: "adidas"
     }
+    /*{
+        id: 1116,
+        title: "Producto 6",
+        description: "",
+        price: '$' + 20000,
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1650084711/adidas-copa-19.1_black-removebg-preview_hyzltp.png",
+        category: "adidas"
+    },
+    {
+        id: 1117,
+        title: "Producto 7",
+        description: "",
+        price: '$' + 20000,
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1650084702/adidas_champions-removebg-preview_mhuugh.png",
+        category: "adidas"
+    },
+    {
+        id: 1118,
+        title: "Producto 8",
+        description: "",
+        price: '$' + 20000,
+        pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1650084702/adidas-copa-19.1-removebg-preview_albyjj.png",
+        category: "adidas"
+    }*/
 ]
 
 export default Products;
