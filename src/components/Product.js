43af0adff7c5f106af002183fd0c871705323e0f
christianbrusa <71473938+christianbrusa@users.noreply.github.com>
2022-04-18 00:50:31 -0300

    Elimino listado producto prueba

--- src/components/Product.js
+++
@@ -1,9 +0,0 @@
-const Product = {
-    id: 1111,
-    title: "Producto 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
-    price: '$' + 20000,
-    pictureUrl: "https://res.cloudinary.com/dycoseuyv/image/upload/v1648705117/redobra-removebg-preview_sxlvqd.png"
-}
-
-export default Product;
