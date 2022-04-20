f08c39b282cf4d380adf2d8652cc3e6c5255fbb3
christianbrusa <christianbrusa@hotmail.com>
2022-04-19 23:49:15 -0300

    Al clickear sobre un item lleva a la ruta con su id

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState } from "react";
 import { Button, Card } from 'react-bootstrap';
 import ItemCount from "./ItemCount";
 import Products from "./Products";
+import { Link } from "react-router-dom";
 import "../css/Item.css";
 
 export default function Item({ id, title, description, price, pictureUrl }) {
@@ -16,8 +17,10 @@ export default function Item({ id, title, description, price, pictureUrl }) {
     return (
         //Fragments
         <>
+
             <div className="products">
-                <Card key={id} style={{ width: '14rem' }}>
+                <Link id="clickOnProduct" to={`/item/${id}`}>
+                <Card key={id} style={{}}>
                     <Card.Img variant="top" src={pictureUrl} />
                     <Card.Body>
                         <Card.Title id="title1">{title}</Card.Title>
@@ -25,9 +28,10 @@ export default function Item({ id, title, description, price, pictureUrl }) {
                         <div className="count">
                             <ItemCount initialStock={initialStock} stock={stock} onAdd={onAdd} />
                         </div>
-                        <Button id="addToCart" variant="outline-primary">Agregar al carrito</Button>
+                        <Button id="addToCart" variant="outline-primary" style={{ fontFamily: "Bahnschrift" }}>Agregar al carrito</Button>
                     </Card.Body>
                 </Card>
+                </Link>
             </div>
         </>
     )
