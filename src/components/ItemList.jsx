9791869bcb3db4e6d35e2dcc745c2975a949fffb
christianbrusa <christianbrusa@hotmail.com>
2022-04-19 23:58:14 -0300

    Agrego prop description a Item

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,13 +5,14 @@ import Item from "./Item";
 export default function ItemList({ Products }) {
     return (
         //Fragments
-        <div className= "goncy" style={{display:"grid", gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))"}}>
+        <div className= "goncy" style={{display:"grid", gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))", marginTop:"-20px"}}>
         {Products.map(prod => (
             
             <Item
                 id={prod.id}
                 title={prod.title}
                 price={prod.price}
+                description={prod.description}
                 pictureUrl={prod.pictureUrl}
             />
         ))}
