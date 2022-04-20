7164ce30924dd6117a0f154d8cbb45ec114e9e04
christianbrusa <christianbrusa@hotmail.com>
2022-04-19 23:53:53 -0300

    Remodelo componente ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "../css/ItemDetail.css";
 
 export default function ItemDetail({producto}) {
     return (
         //Fragments
         <>
-            <div className="ProductDetail">
-            <img id= "pictureDetail" src={producto.pictureUrl} alt="" />
-            </div>
-            <h2>{producto.title}</h2>
-            <h4>{producto.price}</h4>
-            <p>{producto.description}</p>
+            <main class="container">
+                <div class="left-column">
+                    <img src={producto.pictureUrl} alt="" />
+                </div>
+                <div id="right" class="right-column">
+                    <h2>{producto.title}</h2>
+                    <br></br>
+                    <h4>{producto.price}</h4>
+                    <br></br>
+                    <p>{producto.description}</p>
+                    <br></br>
+                    <Button variant="outline-primary" style={{ fontFamily: "Bahnschrift" }}>Agregar al carrito</Button>
+                </div>
+            </main>
         </>
     )
 } 
