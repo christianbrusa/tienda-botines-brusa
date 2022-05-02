52937e7ae6fccdb96fa17735f9b2d32afe5c665b
christianbrusa <christianbrusa@hotmail.com>
2022-05-02 02:16:49 -0300

    Agrego componente ItemNotFound

---
+++ src/components/ItemNotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/ItemNotFound.css";
+
+export default function ItemNotFound() {
+    return (
+        //Fragments
+        <>
+            <div id="error">
+                <div class="errorText">
+                    <h1>Error 404</h1>
+                    <h4>Página no encontrada.</h4>
+                </div>
+            </div>
+        </>
+    )
+}
