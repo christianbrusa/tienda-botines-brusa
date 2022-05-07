e03097ce4039d8888d268b00e98ee8138cd4b515
christianbrusa <christianbrusa@hotmail.com>
2022-05-07 18:53:05 -0300

    Importo SortItems y le paso por props funciones para filtrar items por precio

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
+import SortItems from "./SortItems";
 /*import "../css/";*/
 
-export default function ItemList({ Products }) {
+export default function ItemList({Products}) {
+
+    const [filtroPrecioMenor, setFiltroPrecioMenor] = useState([]);
+    const [filtroPrecioMayor, setFiltroPrecioMayor] = useState([]);
+
+    function mayorPrecio(){
+        let filtroMayor = Products.sort((a, b) => (a.price > b.price ? -1 : 1));
+        setFiltroPrecioMayor(filtroMayor);
+        console.log(filtroMayor);
+    }
+
+    function menorPrecio(){
+        let filtroMenor = Products.sort((a, b) => (a.price > b.price ? 1 : -1));
+        setFiltroPrecioMenor(filtroMenor);
+        console.log(filtroMenor);
+    }
+
     return (
         //Fragments
+        <>
+        <SortItems menorPrecio={menorPrecio} mayorPrecio={mayorPrecio}/>
         <div className= "goncy" style={{display:"grid", gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr))", marginTop:"-20px"}}>
         {Products.map(prod => (
-            
             <Item
                 id={prod.id}
                 title={prod.title}
@@ -17,5 +35,6 @@ export default function ItemList({ Products }) {
             />
         ))}
         </div>
+        </>
     )
 } 
