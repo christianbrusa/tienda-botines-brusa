b417224aab9bfc00040c4212afcc7a23f1ba1581
christianbrusa <christianbrusa@hotmail.com>
2022-05-16 23:37:19 -0300

    Agrego componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+    
+    return (
+        //Fragments
+        <>
+            <img
+                src="https://res.cloudinary.com/dycoseuyv/image/upload/v1652672317/solid1_bnscqs.jpg"
+                width="100%"
+                height="350"
+                className="footer"
+                alt=""
+            />
+        </>
+    )
+}
