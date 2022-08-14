dd4832db0c49192347b96af784537cf3a557fbb9
christianbrusa <christianbrusa@hotmail.com>
2022-08-14 01:30:28 -0300

    Agrego boton eliminar y uso removeItem()

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,35 +5,42 @@ import "../css/Cart.css";
 
 export default function Cart() {
 
-    let { cart } = useContext(Context)
+    let { cart, removeItem } = useContext(Context)
     //console.log(cart)
+
     return (
         //Fragments
         <>
-            <div className='mx-5'>
-                <Container fluid>
-                    <Row id="first-row">
-                        <Col md={5}><h6>Producto</h6></Col>
-                        <Col><h6>Precio</h6></Col>
-                        <Col><h6>Cantidad</h6></Col>
-                        <Col><h6>Subtotal</h6></Col>
-                    </Row>
-                </Container>
-                <Container fluid>
-                    {
-                        cart.map(function (item, index) {
-                            return <Row id="product-row">
-                                <Col md={5}>
-                                    <img id="img-cart" src={cart[index].picture}/>
-                                    <a>{cart[index].title}</a>
-                                </Col>
-                                <Col>{cart[index].price}</Col>
-                                <Col>{cart[index].quantity}</Col>
-                                <Col>{"$" + cart[index].price.split("$")[1] * Number(cart[index].quantity)}</Col>
-                            </Row>
-                        })
-                    }
-                </Container>
+            <div class="row g-0" className="product-list">
+                <div class="col-sm-6 col-md-8">
+                    <Container fluid>
+                        <Row id="first-row">
+                            <Col md={5}><h6>Producto</h6></Col>
+                            <Col><h6>Precio</h6></Col>
+                            <Col><h6>Cantidad</h6></Col>
+                            <Col><h6>Subtotal</h6></Col>
+                            <Col></Col>
+                        </Row>
+                        {
+                            cart.map(function (item, index) {
+                                return <Row id="product-row">
+                                    <Col md={5}>
+                                        <img id="img-cart" src={cart[index].picture}/>
+                                        <a>{cart[index].title}</a>
+                                    </Col>
+                                    <Col>{cart[index].price}</Col>
+                                    <Col>{cart[index].quantity}</Col>
+                                    <Col>{"$" + cart[index].price.split("$")[1] * Number(cart[index].quantity)}</Col>
+                                    <Col>
+                                        <button onClick={() => removeItem(cart[index].item)}>
+                                            <img id="delete-icon" src="https://res.cloudinary.com/dycoseuyv/image/upload/v1660421069/basura-icon_rtsfvm.png" />
+                                        </button>
+                                    </Col>
+                                </Row>
+                            })
+                        }
+                    </Container>
+                </div>
             </div>
         </>
     )
