746ee656dc1a7b05352fa2bf0d8380a3a43909db
christianbrusa <christianbrusa@hotmail.com>
2022-08-18 23:49:40 -0300

    Sumo logica para que no permita agregar items duplicados

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -7,7 +7,8 @@ export default function CartContext({children}) {
     const [cart, setCart] = useState([]);
 
     function addItem(item, title, quantity, price, picture){
-        setCart([...cart, {item, title, quantity, price, picture}])
+        let seEncuentra = cart.some(e => e.item == item);
+        seEncuentra ? window.alert("Este producto ya se encuentra cargado") : setCart([...cart, {item, title, quantity, price, picture}]);
     }
 
     function removeItem(itemId){
