5449cbdea46428b3e7734cf8d3c500e689d4255e
christianbrusa <christianbrusa@hotmail.com>
2022-09-05 22:40:10 -0300

    Guardo en estado el valor ingresado en el input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/SearchBar.css";
 
 export default function SearchBar() {
+
+    const [text, setText] = useState("");
+
+    function filterText(e){
+        setText(e.target.value);
+    }
+
+    console.log(text);
 
     return (
         //Fragments
         <>
             <div class="search-bar">
-                <input type="search" class="form-control" placeholder="Buscar" />
+                <input type="search" class="form-control" placeholder="Buscar" onChange={(e) => filterText(e)}/>
             </div>
         </>
     )
