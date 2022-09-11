88555f2a9f27a2851e516da0354e9ac1662560cf
christianbrusa <christianbrusa@hotmail.com>
2022-09-11 01:53:39 -0300

    Agrego ruta catalog en App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route exact path='*' element={<ItemNotFound />}/>
         <Route exact path='/' element={<ItemListContainer />}/>
         <Route exact path='/category/:id' element={<ItemListContainer />}/>
+        <Route exact path='/catalog' element={<ItemListContainer />}/>
         <Route exact path='/item/:id' element={<ItemDetailContainer />}/>
         <Route exact path='/cart' element={<Cart />}/>
         </Routes>
